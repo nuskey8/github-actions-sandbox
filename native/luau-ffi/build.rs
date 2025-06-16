f24daeef1c1b8169a411a2b6b503a5fdee28f3f2
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-06-16 22:26:28 +0900

    update

--- native/luau-ffi/build.rs
+++ native/luau-ffi/build.rs
@@ -1,9 +1,9 @@
 fn main() {
-    cmake::Config::new("../../luau")
+    new_cnake_config()
         .build_target("Luau.Compiler")
         .build();
 
-    let dst = cmake::Config::new("../../luau")
+    let dst = new_cnake_config()
         .build_target("Luau.Require")
         .build();
 
@@ -26,80 +26,6 @@ fn main() {
         .unwrap()
         .write_to_file("src/luau.rs")
         .unwrap();
-
-    let cs = new_csbindgen_builder("src/luau.rs")
-        .rust_file_header("use super::luau::*;")
-        .csharp_file_header(
-            "
-using lua_newstate_f_delegate = Luau.Native.lua_Alloc;
-using lua_pushcclosurek_fn__delegate = Luau.Native.lua_CFunction;
-using lua_tocfunction_return_delegate = Luau.Native.lua_CFunction;
-using lua_pushcclosurek_cont_delegate = Luau.Native.lua_Continuation;
-using lua_setuserdatadtor_dtor_delegate = Luau.Native.lua_Destructor;
-using lua_getuserdatadtor_return_delegate = Luau.Native.lua_Destructor;
-using lua_getallocf_return_delegate = Luau.Native.lua_Alloc;
-using lua_getcoverage_callback_delegate = Luau.Native.lua_Coverage;
-",
-        );
-
-    cs.generate_to_file(
-        "src/luau_ffi.rs",
-        "../../src/Luau.Native/NativeMethods.g.cs",
-    )
-    .unwrap();
-
-    cs.csharp_dll_name_if(
-        "UNITY_IOS && !UNITY_EDITOR",
-        "__Internal",
-    )
-    .generate_csharp_file("../../src/Luau.Unity/Assets/Luau.Unity/Native/NativeMethods.g.cs")
-    .unwrap();
-
-    bindgen::Builder::default()
-        .headers(["../../luau/Require/Runtime/include/Luau/Require.h"])
-        .clang_arg("-x")
-        .clang_arg("c++")
-        .clang_arg("-std=c++17")
-        .clang_arg("-I../../luau/Compiler/include")
-        .clang_arg("-I../../luau/VM/include")
-        .clang_arg("-I../../luau/Require/Runtime/include")
-        .clang_arg("-DLUA_API=extern\"C\"")
-        .allowlist_function("luarequire_.*")
-        .allowlist_function("luaopen_require")
-        .allowlist_type("luarequire_.*")
-        .blocklist_type("lua_State")
-        .raw_line("use super::luau::*;")
-        .generate()
-        .unwrap()
-        .write_to_file("src/luau_require.rs")
-        .unwrap();
-
-    let cs2 = new_csbindgen_builder("src/luau_require.rs")
-        .rust_file_header(
-            "
-use super::luau::*;
-use super::luau_require::*;
-",
-        )
-        .csharp_disable_emit_dll_name(true)
-        .csharp_file_header(
-            "
-using luarequire_pushrequire_config_init_delegate = Luau.Native.luarequire_Configuration_init;     
-using luaopen_require_config_init_delegate = Luau.Native.luarequire_Configuration_init;    
-using luarequire_pushproxyrequire_config_init_delegate = Luau.Native.luarequire_Configuration_init;   
-",
-        );
-
-    cs2.generate_to_file(
-        "src/luau_require_ffi.rs",
-        "../../src/Luau.Native/NativeMethods.Require.g.cs",
-    )
-    .unwrap();
-
-    cs2.generate_csharp_file(
-        "../../src/Luau.Unity/Assets/Luau.Unity/Native/NativeMethods.Require.g.cs",
-    )
-    .unwrap();
 }
 
 fn new_csbindgen_builder(src: &'static str) -> csbindgen::Builder {
@@ -113,4 +39,18 @@ fn new_csbindgen_builder(src: &'static str) -> csbindgen::Builder {
         .csharp_class_accessibility("public")
         .csharp_generate_const_filter(|x| x.starts_with("LUA"))
         .csharp_use_function_pointer(false)
+}
+
+fn new_cnake_config() -> cmake::Config {
+    let mut config = cmake::Config::new("../../luau");
+
+    let target = build_target::target_triple().unwrap();
+    if target == "aarch64-unknown-linux-gnu" {
+        config.define("CMAKE_SYSTEM_NAME", "Linux");
+        config.define("CMAKE_SYSTEM_PROCESSOR", "aarch64");
+        config.define("CMAKE_C_COMPILER", "aarch64-linux-gnu-gcc");
+        config.define("CMAKE_CXX_COMPILER", "aarch64-linux-gnu-g++");
+    }
+
+    return config;
 }
