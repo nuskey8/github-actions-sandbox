2af9182813f35fd2c14a91cadd4a142c7ed20215
nuskey8 <84110981+nuskey8@users.noreply.github.com>
2025-06-16 22:30:28 +0900

    f

--- native/luau-ffi/src/lib.rs
+++ native/luau-ffi/src/lib.rs
@@ -8,19 +8,7 @@ mod luau;
 #[allow(non_snake_case)]
 #[allow(non_camel_case_types)]
 #[allow(non_upper_case_globals)]
-mod luau_ffi;
-
-#[allow(dead_code)]
-#[allow(non_snake_case)]
-#[allow(non_camel_case_types)]
-#[allow(non_upper_case_globals)]
 mod luau_require;
-
-#[allow(dead_code)]
-#[allow(non_snake_case)]
-#[allow(non_camel_case_types)]
-#[allow(non_upper_case_globals)]
-mod luau_require_ffi;
 
 #[cfg(test)]
 mod test {
